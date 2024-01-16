940be37b0fb10ff0c0fabd01f8798525e20b21c5
QoidynBasy <c0mplexie1@gmail.com>
2024-01-16 14:22:26 +0600

    second

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 1337;
-
+const dir = __dirname+'/frontend/html/';
 app.get(('/'),(req, res) => {
-    res.send(')))')
+    res.sendFile(dir + 'main.html');
 });
 
+app.post('/add', (req, res) => {
+
+});
 app.listen(port, () => {
     console.log(`app is working on port -> ${port}`);
 });
